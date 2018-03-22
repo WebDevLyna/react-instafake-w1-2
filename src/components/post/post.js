96846c747c6df1310f1ceaf75b8111cb1f455818
Graphic <graphic@SOD-SJH-603-28M.local>
2018-03-22 19:17:07 -0400

    Adding some CSS classes and styles to post

--- src/components/post/post.js
+++ src/components/post/post.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './post.css';
 
 export const Post = () => (
-  <div>
-    <div className="image">header</div>
-    <img className="postImg" src="https://cdn.vox-cdn.com/thumbor/2SzuuGQHPaDvQCgyy_vKB4BGN60=/0x0:2040x1360/920x613/filters:focal(857x517:1183x843):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/57516461/hero.0.0.jpg" alt="" />
-    <div>comment section</div>
+  <div className="post">
+    <div className="post__header">header</div>
+    <img className="post__img" src="https://cdn.vox-cdn.com/thumbor/2SzuuGQHPaDvQCgyy_vKB4BGN60=/0x0:2040x1360/920x613/filters:focal(857x517:1183x843):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/57516461/hero.0.0.jpg" alt="" />
+    <div className="post__comment">comment section</div>
   </div>
 );
