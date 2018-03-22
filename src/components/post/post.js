1d822b5e35489c80e7b33059ba4250bee40253fd
Graphic <graphic@SOD-SJH-603-28M.local>
2018-03-22 19:05:37 -0400

    Import post.css in post.js

--- src/components/post/post.js
+++ src/components/post/post.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import './post.css';
 
 export const Post = () => (
-  <div> Test Ya! </div>
+  <div>
+    <div className="image">header</div>
+    <img className="postImg" src="https://cdn.vox-cdn.com/thumbor/2SzuuGQHPaDvQCgyy_vKB4BGN60=/0x0:2040x1360/920x613/filters:focal(857x517:1183x843):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/57516461/hero.0.0.jpg" alt="" />
+    <div>comment section</div>
+  </div>
 );
