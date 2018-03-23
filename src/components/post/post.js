f00dc6bf74634a528142dc7b77edbef510350925
Graphic <graphic@SOD-SJH-603-28M.local>
2018-03-22 21:41:45 -0400

    Map through userComment from comments array object

--- src/components/post/post.js
+++ src/components/post/post.js
@@ -4,10 +4,19 @@ import './post.css';
 export const Post = (props) => (
   <div className="post">
     <div className="post__header">
+      <img className="post__profile-img" src={props.profileUrl} />
       <b>{props.owner}</b>
       <br/>{props.location}
     </div>
     <img className="post__img" src={props.imageUrl} alt="" />
-    <div className="post__comment">comment section</div>
+    <div className="post__comment">
+      <p><b>{props.owner}</b></p>
+      <p>{props.comments.map((userComment) => (
+          <div>
+            <p><b>{userComment.owner}</b>: {userComment.text}</p>
+          </div>
+        ))}
+      </p>
+    </div>
   </div>
 );
--- src/components/timeline/timeline.js
+++ src/components/timeline/timeline.js
@@ -2,22 +2,55 @@ import React from 'react';
 import { Post } from '../post';
 import './timeline.css';
 
-const photos = [
+const posts = [
   {
     //Object array
+    profileUrl: "https://pbs.twimg.com/profile_images/430583461909233665/Qka1Zm_o_400x400.jpeg",
     owner: 'sweetnamese',
     location: "Toronto, ON",
-    imageUrl: "https://cdn.vox-cdn.com/thumbor/2SzuuGQHPaDvQCgyy_vKB4BGN60=/0x0:2040x1360/920x613/filters:focal(857x517:1183x843):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/57516461/hero.0.0.jpg"
+    imageUrl: "https://cdn.vox-cdn.com/thumbor/2SzuuGQHPaDvQCgyy_vKB4BGN60=/0x0:2040x1360/920x613/filters:focal(857x517:1183x843):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/57516461/hero.0.0.jpg",
+    comments: [
+      {
+        owner: 'tom',
+        text: "This is comment number 1"
+      },
+      {
+        owner: 'jerry',
+        text: "I like your photo"
+      }
+    ]
   },
   {
-    owner: 'stacy',
-    location: "Brampton, ON",
-    imageUrl: "https://www.omnicoreagency.com/wp-content/uploads/2015/10/Instagram.jpg"
+    profileUrl: "https://assets.teenvogue.com/photos/583345cc5e8102342c84bc39/master/w_757,c_limit/giphy-6.gif",
+    owner: 'teklyn',
+    location: "Scarborough, ON",
+    imageUrl: "https://www.omnicoreagency.com/wp-content/uploads/2015/10/Instagram.jpg",
+    comments: [
+      {
+        owner: 'tom',
+        text: "This is comment number 1"
+      },
+      {
+        owner: 'jerry',
+        text: "I like your photo"
+      }
+    ]
   },
   {
+    profileUrl: "http://www.geocities.ws/silver_icecrystal/tuxmaskwroses.jpg",
     owner: 'antonio',
     location: "Woodbridge, ON",
-    imageUrl: "https://i.amz.mshcdn.com/PRqWLssePCJ0AknJV5dmnTeiaKQ=/950x534/filters:quality(90)/https%3A%2F%2Fblueprint-api-production.s3.amazonaws.com%2Fuploads%2Fcard%2Fimage%2F484914%2F79d9fa6b-11dd-4dda-b2dc-9d4feed74f8e.jpg"
+    imageUrl: "https://i.amz.mshcdn.com/PRqWLssePCJ0AknJV5dmnTeiaKQ=/950x534/filters:quality(90)/https%3A%2F%2Fblueprint-api-production.s3.amazonaws.com%2Fuploads%2Fcard%2Fimage%2F484914%2F79d9fa6b-11dd-4dda-b2dc-9d4feed74f8e.jpg",
+    comments: [
+      {
+        owner: 'tom',
+        text: "This is comment number 1"
+      },
+      {
+        owner: 'jerry',
+        text: "I like your photo"
+      }
+    ]
   },
 ];
 
@@ -26,15 +59,17 @@ export const Timeline = () => (
     <div className="timeline__post-container">
 
       {/* Map loops the object */}
-      {photos.map(post => (
+      {posts.map(post => (
         <div className="timeline__post">
           <Post
+            profileUrl={post.profileUrl}
             owner={post.owner}
             location={post.location}
             imageUrl={post.imageUrl}
+            comments={post.comments}
           />
         </div>
-      ))};
+      ))}
 
     </div>
   </div>
