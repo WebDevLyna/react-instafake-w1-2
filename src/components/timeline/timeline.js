c1960c3b5633119c1d5c42c253821b6fb6fc9935
Graphic <graphic@SOD-SJH-603-28M.local>
2018-03-22 20:15:50 -0400

    Adding an array of photos to simplify and clean up timeline.js

--- src/components/timeline/timeline.js
+++ src/components/timeline/timeline.js
@@ -2,27 +2,25 @@ import React from 'react';
 import { Post } from '../post';
 import './timeline.css';
 
+const photos = [
+  <div className="timeline__post">
+    <Post
+      owner="WebDevLyna"
+      location="Toronto, ON"
+      imageUrl="https://cdn.vox-cdn.com/thumbor/2SzuuGQHPaDvQCgyy_vKB4BGN60=/0x0:2040x1360/920x613/filters:focal(857x517:1183x843):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/57516461/hero.0.0.jpg" />
+  </div>,
+  <div className="timeline__post">
+    <Post
+      owner="WebDevLyna"
+      location="Toronto, ON"
+      imageUrl="https://cdn.vox-cdn.com/thumbor/2SzuuGQHPaDvQCgyy_vKB4BGN60=/0x0:2040x1360/920x613/filters:focal(857x517:1183x843):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/57516461/hero.0.0.jpg" />
+  </div>
+];
+
 export const Timeline = () => (
   <div className="timeline">
     <div className="timeline__post-container">
-      <div className="timeline__post">
-      <Post
-        owner="WebDevLyna"
-        location="Toronto, ON"
-        imageUrl="https://cdn.vox-cdn.com/thumbor/2SzuuGQHPaDvQCgyy_vKB4BGN60=/0x0:2040x1360/920x613/filters:focal(857x517:1183x843):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/57516461/hero.0.0.jpg" />
-      </div>
-      <div className="timeline__post">
-      <Post
-        owner="WebDevLyna"
-        location="Toronto, ON"
-        imageUrl="https://cdn.vox-cdn.com/thumbor/2SzuuGQHPaDvQCgyy_vKB4BGN60=/0x0:2040x1360/920x613/filters:focal(857x517:1183x843):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/57516461/hero.0.0.jpg" />
-      </div>
-      <div className="timeline__post">
-      <Post
-        owner="WebDevLyna"
-        location="Toronto, ON"
-        imageUrl="https://cdn.vox-cdn.com/thumbor/2SzuuGQHPaDvQCgyy_vKB4BGN60=/0x0:2040x1360/920x613/filters:focal(857x517:1183x843):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/57516461/hero.0.0.jpg" />
-      </div>
+      {photos}
     </div>
   </div>
 );
